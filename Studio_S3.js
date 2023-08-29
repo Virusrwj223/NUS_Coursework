7efe7251894bf3d0717ad4e9e577116808e8f19a
Source Academy User <No public email provided>
2023-08-29 21:55:04 +0800

    EXTRA PRAC QNS

--- Studio_S3.js
+++ Studio_S3.js
@@ -34,3 +34,37 @@ iterative process. time = O(n), space O(n). size refers to the value of the
 integer. The assumptiopn is that calling each rune operation and primitive 
 conumes negligable resources.
 */
+
+//////////////////////////////////////////////////////////////////////////////////////
+//EXTRA PRAC//
+//////////////////////////////////////////////////////////////////////////////////////
+
+function fast_expt(b,n){
+    return n===1
+        ? b
+        :
+        n%2===0
+        ? fast_expt(b,n/2)*fast_expt(b,n/2)
+        : b*fast_expt(b,n-1);
+}
+
+function iter(b,n){
+
+    function fast_expt_iter(b,n,counter,current_answer){
+        
+        return n-counter===0
+            ? current_answer
+            : n-counter===1
+            ? b*current_answer
+            : counter%2===1
+            ? fast_expt_iter(b,n,counter*2,current_answer*current_answer)
+            :fast_expt_iter(b,n,counter+1,current_answer*b);
+            
+    }
+        
+    
+    return fast_expt_iter(b,n,1,b);
+    
+}
+
+display(iter(3,6));
